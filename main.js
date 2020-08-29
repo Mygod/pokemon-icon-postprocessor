27837f7c6b16a3e796db410bdf4cbbe775d9930d
Mygod <contact-git@mygod.be>
2020-08-29 19:07:43 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,154 @@
+const child_process = require("child_process");
+const fs = require('fs');
+const path = require('path');
+
+const POGOProtos = require('pogo-protos');
+const gameMaster = require('pokemongo-game-master');
+
+function createFormTargets(formLookup, suffix) {
+    let formTargets = formLookup[suffix];
+    if (formTargets === undefined) {
+        return formLookup[suffix] = {
+            targets: [],
+            female: true
+        };
+    } else {
+        console.warn('Multiple targets found for asset', suffix);
+        return formTargets;
+    }
+}
+
+function extractFormTargets(formLookup, template, pokemonId, computeSuffix, separator = '_') {
+    const pokemonIdString = String(pokemonId).padStart(3, '0');
+    const formSettings = template.data[Object.keys(template.data)[1]];
+    let forms = formSettings[Object.keys(formSettings)[1]];
+    if (forms === undefined) {
+        let pokemon = template.templateId.split('_POKEMON_', 2)[1];
+        forms = [{
+            "form": pokemon + "_NORMAL"
+        }];
+    }
+    for (const formData of forms) {
+        const keys = Object.keys(formData);
+        const form = formData[keys[0]];
+        if (form.endsWith("_SHADOW") || form.endsWith("_PURIFIED")) {
+            continue;
+        }
+        const formId = computeSuffix(form);
+        if (!formId) {
+            console.warn('Unrecognized form/temp evolution', form);
+            continue;
+        }
+        const target = pokemonIdString + separator + formId;
+        let assetBundleSuffix = formData[keys[1]] || 0;
+        if (Number.isInteger(assetBundleSuffix)) {  // is actually assetBundleValue
+            if (assetBundleSuffix === 0) {
+                createFormTargets(formLookup, pokemonIdString + '_01').targets.push(target + '_female');
+            }
+            assetBundleSuffix = pokemonIdString + '_' + String(assetBundleSuffix).padStart(2, '0');
+        } else if (assetBundleSuffix.indexOf('_00_') >= 0) {
+            createFormTargets(formLookup, assetBundleSuffix.replace('_00_', '_01_')).targets.push(target + '_female');
+        }
+        let formTargets = createFormTargets(formLookup, assetBundleSuffix);
+        formTargets.targets.push(target);
+        formTargets.female = false;
+    }
+}
+
+(async () => {
+    let inDir = process.argv[2];
+    let outDir = process.argv[3];
+    if (!inDir || !outDir) {
+        console.error('Usage: node main.js <input dir> <output dir>');
+        return;
+    }
+    inDir = path.resolve(inDir);
+    outDir = path.resolve(outDir);
+
+    console.log('Reading game master...');
+    const formLookup = {
+        '000': {    // substitute is not in gameMaster
+            targets: ['000']
+        }
+    };
+    const gameMasterContent = await gameMaster.getVersion('latest', 'json');
+    for (const template of gameMasterContent.template) {
+        if (template.templateId.startsWith('FORMS_V')) {
+            const pokemonId = parseInt(template.templateId.substr(7, 4));
+            if (!pokemonId) {
+                console.warn('Unrecognized templateId', template.templateId);
+            } else {
+                extractFormTargets(formLookup, template, pokemonId, (form) => POGOProtos.Enums.Form[form]);
+            }
+        } else if (template.templateId.startsWith('TEMPORARY_EVOLUTION_V')) {
+            const pokemonId = parseInt(template.templateId.substr(21, 4));
+            if (!pokemonId) {
+                console.warn('Unrecognized templateId', template.templateId);
+            } else {
+                extractFormTargets(formLookup, template, pokemonId, (evolution) => {
+                    // <RANDOMIZED_NAME>_TEMP_EVOLUTION_<NAME> => EVOLUTION_<NAME>
+                    return POGOProtos.Enums.PokemonEvolution[evolution.split('_TEMP_', 2)[1]];
+                }, '_v');
+            }
+        }
+    }
+    for (const suffix1 of Object.keys(formLookup)) {
+        for (const suffix2 of Object.keys(formLookup)) {
+            if (suffix1 !== suffix2 && (suffix1.startsWith(suffix2) || suffix2.startsWith(suffix1))) {
+                console.error('Illegal combinations found', suffix1, suffix2);
+                return;
+            }
+        }
+    }
+
+    await fs.promises.mkdir(outDir, { recursive: true });
+    for (const filename of await fs.promises.readdir(inDir)) {
+        if (!filename.startsWith('pokemon_icon_')) {
+            continue;
+        }
+        const name = filename.substr(13);
+        let suffix;
+        let formTargets = null;
+        for (const [prefix, data] of Object.entries(formLookup)) {
+            if (name.startsWith(prefix)) {
+                suffix = name.substr(prefix.length);
+                formTargets = data;
+                break;  // we can break since we have done the check
+            }
+        }
+        if (formTargets === null) {
+            console.warn('Unrecognized/unused asset', filename);
+            continue;
+        }
+        formTargets.hit = true;
+        let output = null;
+        for (const target of formTargets.targets) {
+            const targetPath = path.join(outDir, 'pokemon_icon_' + target + suffix);
+            if (output === null) {
+                const sourcePath = path.join(inDir, filename);
+                const child = child_process.spawnSync('convert', ['-trim', '-fuzz', '1%', sourcePath, targetPath], {
+                    stdio: 'inherit'
+                });
+                if (child.error || child.status) {
+                    console.error('Failed to convert', sourcePath, 'exited with', child.status, child.error);
+                } else {
+                    output = targetPath;
+                }
+            } else {
+                await fs.promises.copyFile(output, targetPath);
+            }
+        }
+    }
+
+    const female = [];
+    for (const [suffix, data] of Object.entries(formLookup)) {
+        if (data.hit) {
+            for (const entry of data.targets.filter((target) => target.endsWith('_female'))) {
+                female.push(entry.substr(0, entry.length - 7));
+            }
+        } else if (!data.female) {
+            console.warn('Found form/temporary evolution with no matching assets', suffix, data);
+        }
+    }
+    console.log('female =', female);
+})();
